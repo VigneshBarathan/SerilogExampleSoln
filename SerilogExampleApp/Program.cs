4f42fabb65a2877f294fcd6a45af370279c34c29
Vignesh Barathan <vignesh.barathan@sisystems.com>
2022-01-10 21:34:34 +0530

    Add project files.

---
+++ SerilogExampleApp/Program.cs
@@ -0,0 +1,34 @@
+﻿using Serilog;
+using System;
+
+namespace SerilogExampleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Serilogfunc();
+        }
+
+        private static void Serilogfunc()
+        {
+            Log.Logger = new LoggerConfiguration().WriteTo.File(@"C:\Misc\Logs\Serilog\logfile.log").CreateLogger();
+            Log.Information("This is a sample information!.");
+            try
+            {
+                int a = 2;
+                int b = 0;
+                Log.Debug("The values are {0} and {0}", a, b);
+                int c = a / b;
+            }
+            catch (System.Exception ex)
+            {
+                Log.Error(ex, "Some error occurred");
+            }
+
+            Log.CloseAndFlush();
+            Console.WriteLine("Completed");
+            Console.ReadLine();
+        }
+    }
+}
